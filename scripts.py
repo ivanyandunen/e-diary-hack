af658bc76058fd4114f5a590d4ac2d0e01708ccc
Ivan Yandunen <ivan.yandunen@gmail.com>
2020-01-11 22:14:39 +0300

    Initial commit

---
+++ scripts.py
@@ -0,0 +1,60 @@
+import random
+from datacenter.models import Schoolkid, Lesson, Mark, Chastisement, Commendation
+from django.core.exceptions import MultipleObjectsReturned, ObjectDoesNotExist
+
+
+def get_schoolkid(name):
+    try:
+        schoolkid = Schoolkid.objects.get(full_name__contains=name)
+        return schoolkid
+    except MultipleObjectsReturned:
+        print('Several schoolkids with specified name were found')
+    except ObjectDoesNotExist:
+        print('There is no schoolkid with specified name')
+
+
+def fix_marks(name):
+    schoolkid = get_schoolkid(name)
+    marks = Mark.objects.filter(schoolkid=schoolkid, points__lt=4)
+    for mark in marks:
+        mark.points = 5
+        mark.save()
+
+
+def remove_chastisements(name):
+    schoolkid = get_schoolkid(name)
+    chastisements = Chastisement.objects.filter(schoolkid=schoolkid)
+    chastisements.delete()
+
+
+def get_commendation():
+    commendation_list = [
+        'Молодец!',
+        'Отлично!',
+        'Хорошо!',
+        'Ты меня приятно удивил!',
+        'Великолепно!',
+        'Очень хороший ответ!',
+        'Замечательно!',
+        'Так держать!',
+        'Я вижу, как ты стараешься!',
+        'Талантливо!',
+    ]
+    return random.choice(commendation_list)
+
+
+def create_commendation(name, subject):
+    schoolkid = get_schoolkid(name)
+    lesson = Lesson.objects.filter(
+        year_of_study=schoolkid.year_of_study,
+        group_letter=schoolkid.group_letter,
+        subject__title=subject
+        ).order_by('-date')[0]
+
+    Commendation.objects.create(
+        text=get_commendation(),
+        created=lesson.date,
+        schoolkid=schoolkid,
+        subject=lesson.subject,
+        teacher=lesson.teacher
+        )
